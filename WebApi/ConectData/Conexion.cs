eb28a5f0036a27fd99d0b3d19ad5c9350781bed4
Julian Colmenares <Julian Colmenares@JCOLMENARES>
2021-07-31 18:43:08 -0600

    Agregar archivos de proyecto.

---
+++ WebApi/ConectData/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Data
+{
+    public class Conexion
+    {
+        public static string rutaConexion = "Data Source=JCOLMENARES\\SQLEXPRESS;Initial Catalog=EMPRESA;Integrated Security=True";
+    }
+}
